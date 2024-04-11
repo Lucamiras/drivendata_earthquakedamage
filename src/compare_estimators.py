f8489327b721be80df2fd9e839ae34d1bfd95d19
Luc <lucamiras@googlemail.com>
2024-04-11 23:38:05 +0200

    Did a full CV Grid Search for the Random Forest, tried additional boosters

--- src/compare_estimators.py
+++
@@ -1,7 +0,0 @@
-from sklearn.model_selection import cross_val_score
-
-def compare_estimators(estimators:tuple, X, y):
-    # Iterate over estimators and perform cross-validation
-    for name, estimator in estimators:
-        scores = cross_val_score(estimator, X, y, cv=5)
-        print(f"{name}: Accuracy: {scores.mean():.2f} (+/- {scores.std() * 2:.2f})")
---
+++ src/cross_validate.py
@@ -0,0 +1,9 @@
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+
+def compare_estimators(estimators:tuple, X, y):
+    
+    #stratified_kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+    for name, estimator in estimators:
+        scores = cross_val_score(estimator, X, y, cv=5, n_jobs=10)
+        print(f"{name}: Accuracy: {scores.mean():.2f} (+/- {scores.std() * 2:.2f})")
