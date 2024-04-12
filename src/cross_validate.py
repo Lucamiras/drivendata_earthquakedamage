b62e4b1f3f873aa0400ca973a8dde04e200b9b6d
Luc <lucamiras@googlemail.com>
2024-04-12 14:26:43 +0200

    Encoded geo_level_1_id and reran model training

--- src/cross_validate.py
+++ src/cross_validate.py
@@ -5,5 +5,5 @@ def compare_estimators(estimators:tuple, X, y):
     #stratified_kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
     for name, estimator in estimators:
-        scores = cross_val_score(estimator, X, y, cv=5, n_jobs=10)
+        scores = cross_val_score(estimator, X, y, cv=5, n_jobs=10, scoring='f1_micro')
         print(f"{name}: Accuracy: {scores.mean():.2f} (+/- {scores.std() * 2:.2f})")
--- src/encode_features.py
+++ src/encode_features.py
@@ -1,14 +1,6 @@
 import pandas as pd
 
-def encode_features(df):
-    non_numerical_columns = ["land_surface_condition",
-        "foundation_type",
-        "roof_type",
-        "ground_floor_type",
-        "other_floor_type",
-        "position",
-        "plan_configuration",
-        "legal_ownership_status"]
+def encode_categorical_features(df, non_numerical_columns):
     encoded_features = pd.get_dummies(df[non_numerical_columns])
     numerical_features = df.drop(non_numerical_columns, axis=1)
     new_df = pd.concat([encoded_features,numerical_features],axis=1)
